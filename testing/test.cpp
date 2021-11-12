9073c9ebaed9dda557c61a5905c852abc9977918
Chinmay Todankar <ctodankar@wpi.edu>
2021-11-12 18:34:59 -0500

    Added Flann Matching Code

--- testing/test.cpp
+++ testing/test.cpp
@@ -85,14 +85,17 @@ void getKeyPoints(Mat inputImage, vector<KeyPoint>& keyPoints) {
 
 int main(int argc, char **argv) {
 
-    Mat left = imread("testing/images/left.jpeg");
-    Mat right = imread("testing/images/right.jpeg");
+    Mat left = imread("testing/images/c.jpeg", IMREAD_GRAYSCALE);
+    Mat right = imread("testing/images/d.jpeg", IMREAD_GRAYSCALE);
+    resize(left, left, Size(left.cols/2, left.rows/2));
+    resize(right, right, Size(right.cols/2, right.rows/2));
+
     vector<KeyPoint> keyPointVector1, keyPointVector2;
     Mat descriptors1,descriptors2;
     Ptr<FeatureDetector> detector = ORB::create();
     Ptr<DescriptorExtractor> descriptor = ORB::create();
-    Ptr<DescriptorMatcher> matcherBrute = DescriptorMatcher::create("BruteForce−Hamming");
-    Ptr<DescriptorMatcher> matcherFlann = DescriptorMatcher::create("FlannBased");
+    Ptr<DescriptorMatcher> matcherBrute = DescriptorMatcher::create("BruteForce-Hamming");
+    FlannBasedMatcher   matcherFlann = FlannBasedMatcher(makePtr<flann::LshIndexParams>(12, 20, 2));
     chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
     detector->detect(left, keyPointVector1);
     detector->detect(right, keyPointVector2);
@@ -105,7 +108,11 @@ int main(int argc, char **argv) {
 
     Mat outimg1;
     drawKeypoints(left, keyPointVector1, outimg1, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
-    imshow("ORB features", outimg1);
+    imshow("ORB features 1", outimg1);
+
+    Mat outimg2;
+    drawKeypoints(right, keyPointVector2, outimg2, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+    imshow("ORB features 2", outimg2);
 
     vector<DMatch> matchesBrute, matchesFlann;
     t1 = chrono::steady_clock::now();
@@ -113,26 +120,30 @@ int main(int argc, char **argv) {
     t2 = chrono::steady_clock::now();
     time_used = chrono::duration_cast<chrono::duration<double> > (t2 - t1);
     cout << "Match Brute cost = " << time_used.count() << " seconds. " << endl;
-
+    cout << "vsbf" << endl;
+    vector<vector<DMatch>> matchesKnnFlann;
+    // vector<DMatch> matchesKnnFlann;
     t1 = chrono::steady_clock::now();
-    matcherFlann->match(descriptors1, descriptors2, matchesFlann);
+    matcherFlann.knnMatch(descriptors1, descriptors2, matchesKnnFlann, 1);
+    // matcherFlann.match(descriptors1, descriptors2, matchesFlann);
+
     t2 = chrono::steady_clock::now();
     time_used = chrono::duration_cast<chrono::duration<double> > (t2 - t1);
-    cout << "Match Brute cost = " << time_used.count() << " seconds. " << endl;
-
+    cout << "Match Flann cost = " << time_used.count() << " seconds. " << endl;
+    cout << "vsbf" << endl;
     auto min_maxBrute = minmax_element(matchesBrute.begin(), matchesBrute.end(), [](const DMatch &m1, const DMatch &m2) { return m1.distance < m2.distance; });
     double min_dist_brute = min_maxBrute.first->distance;
     double max_dist_brute = min_maxBrute.second->distance;
 
-    printf("−− Max dist : %f \n", max_dist_brute);
-    printf("−− Min dist : %f \n", min_dist_brute);
+    printf("−− Max dist Brute: %f \n", max_dist_brute);
+    printf("−− Min dist Brute: %f \n", min_dist_brute);
 
     auto min_maxFlann = minmax_element(matchesFlann.begin(), matchesFlann.end(), [](const DMatch &m1, const DMatch &m2) { return m1.distance < m2.distance; });
     double min_dist_flann = min_maxFlann.first->distance;
     double max_dist_flann = min_maxFlann.second->distance;
 
-    printf("−− Max dist : %f \n", max_dist_flann);
-    printf("−− Min dist : %f \n", min_dist_flann);
+    // printf("−− Max dist Flann: %f \n", max_dist_flann);
+    // printf("−− Min dist Flann: %f \n", min_dist_flann);
 
     vector<DMatch> goodMatchBrute, goodMatchFlann;
 
@@ -140,23 +151,43 @@ int main(int argc, char **argv) {
         if(matchesBrute[i].distance <= max(2 * min_dist_brute, 30.0)) {
             goodMatchBrute.push_back(matchesBrute[i]);
         }
-        if(matchesFlann[i].distance <= max(2 * min_dist_flann, 30.0)) {
-            goodMatchFlann.push_back(matchesFlann[i]);
+    }
+    cout << matchesKnnFlann.size() << endl;
+    size_t size = matchesKnnFlann.size();
+    for(auto i = 0; i < size; i++) {
+        cout << "a";
+        if(matchesKnnFlann[i][0].distance < 0.7f * matchesKnnFlann[i][1].distance) {
+            cout << endl;
+            cout << "a" << endl;
+            goodMatchFlann.push_back(matchesKnnFlann[i][0]);
+            cout << endl;
+            cout << "a" << endl;
         }
     }
-
+    // for(int i = 0; i < matchesFlann.size(); i++) {
+    //     if(matchesFlann[i].distance <= max(2 * min_dist_flann, 30.0)) {
+    //         goodMatchFlann.push_back(matchesFlann[i]);
+    //     }
+    // }
+    // for(auto i = 0; i < matchesKnnFlann.size() - 1; i++) {
+    //     if(matchesKnnFlann[i].distance < 0.5f * matchesKnnFlann[i+1].distance) {
+    //         goodMatchFlann.push_back(matchesKnnFlann[i]);
+    //     }
+    // }
+    // cout << "vsbf" << endl;
+    
     Mat img_matchBrute, img_matchFlann;
     Mat img_goodmatchBrute, img_goodmatchFlann;
     drawMatches(left, keyPointVector1, right, keyPointVector2, matchesBrute, img_matchBrute);
-    drawMatches(left, keyPointVector2, right, keyPointVector2, goodMatchBrute, img_goodmatchBrute);
-    imshow("all matches", img_matchBrute);
-    imshow("good matches", img_goodmatchBrute);
+    drawMatches(left, keyPointVector1, right, keyPointVector2, goodMatchBrute, img_goodmatchBrute);
+    imshow("all matches brute", img_matchBrute);
+    imshow("good matches brute", img_goodmatchBrute);
 
-    drawMatches(left, keyPointVector1, right, keyPointVector2, matchesFlann, img_matchFlann);
-    drawMatches(left, keyPointVector2, right, keyPointVector2, goodMatchFlann, img_goodmatchFlann);
-    imshow("all matches", img_matchFlann);
-    imshow("good matches", img_goodmatchFlann);
-
+    // drawMatches(left, keyPointVector1, right, keyPointVector2, matchesKnnFlann, img_matchFlann);
+    drawMatches(left, keyPointVector1, right, keyPointVector2, goodMatchFlann, img_goodmatchFlann);
+    // imshow("all matches flann", img_matchFlann);
+    imshow("good matches flann", img_goodmatchFlann);
+    cout << "vsbf" << endl;
     // t1 = chrono::steady_clock::now();
     // FAST(left, keyPointVector1, 50);
     
